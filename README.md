c6ecd375e85f09c4db6ff9ff81888004299546c0
Lu Shihan <38780063+Angel-Hair@users.noreply.github.com>
2019-06-30 13:18:57 +0800

    updata cnki & delete taobao
    更新了知网爬虫程序，删掉了弃用的淘宝爬虫程序。

--- README.md
+++ README.md
@@ -2,35 +2,38 @@
 
 这里是我用来存放用于数据分析的爬虫程序的库，实现语言为 Python，测试环境为 window10 专业版，编辑软件为 VS code。
 
-## 程序清单
+因为这些程序都是我在竞赛和做分析时写的，可能比较粗糙，大家见谅（逃）
+
+## 直接对接的爬虫
+
+### cnki
+
+tools: Selenium、Geckodriver、FireFox-headless、PyQt5
+
+这个例子是用来爬取知网上指定关键字搜索的前200页（可改）所有论文的一些信息（标题、作者、摘要、关键字）。**因为用到了 Geckodriver 所以注意程序执行目录下面一定有`geckodriver.exe`文件！**
+
+实现细节： 搜索页面（父级页面）是直接用 Selenium 对接 FireFox-headless 模式实现的，论文详情页面（子级页面）是直接用 requests 和 HTMLParser 库处理的。界面用 PyQt5 多线程做的。
+
+## 基于Scrapy的爬虫
 
 ### chengduqiye
 
 tools：Scarpy
 
-这个例子用的是顺企网的成都信息与计算机企业黄页，由于顺企网没有整合页面，所以如果要爬取其他地域或者行业，需要把'url'改为对应的地址。
+这个例子用的是顺企网的成都信息与计算机企业黄页，由于顺企网没有整合页面，所以如果要爬取其他地域或者行业，需要把 `url`改为对应的地址。
 
-实现细节： 注意使用了Item Pipeline来清理数据。
+实现细节： 注意使用了`Item Pipeline`来清理数据。
 
 ### lagouwangpaqu
 
 tools: Scarpy、Selenium、Geckodriver、FireFox-headless
 
-这个例子用的是拉勾网所有企业的爬取，**注意修改'headers'和设置'start'和'max_list'**，分别对应最低存在企业页面数（2018.11.30测试结果为7页开始有企业）和最高企业数（首页有公布数据），爬取二级信息（招聘）的代码被我注释掉了（包括Downloader Middleware），有需要的可以解开使用。
+这个例子用的是拉勾网所有企业的爬取，**注意修改`headers`和设置`start`和`max_list`**，分别对应最低存在企业页面数（2018.11.30测试结果为7页开始有企业）和最高企业数（首页有公布数据），爬取二级信息（招聘）的代码被我注释掉了（包括Downloader Middleware），有需要的可以解开使用。
 
 值得一提的是拉勾网默认企业名录只显示20页，很明显，没有显示全部，所以这里是直接穷举了所有企业的链接。
 
-实现细节： Scrapy下载中间件（Downloader Middleware）对接Selenium实现的，Selenium驱动FireFox-headless模式，需要额外支持Geckodriver。  
+实现细节： Scrapy下载中间件（Downloader Middleware）对接 Selenium 实现的，Selenium 驱动 FireFox-headless 模式，需要额外支持 Geckodriver 。  
 **PS：PhantomJS已死,有事烧纸。**
-
-### scrapytaobao（！**未被开发完全且不能运行**！）
-
-tools: Scarpy、Selenium、Geckodriver、FireFox-headless
-
-这个例子是本来我用来爬取淘宝各种信息的，后来因为各种原因被弃坑了，仅供各位参考。  
-**另外提醒下，在爬取淘宝这种复杂多级多动态网页时，Selenium和Scarpy往往不是最佳的选择，因为实现起来非常缓慢和复杂，反而直接调用requests要快速方便些，许多请求url可以直接合并得到，唯一麻烦的就是数据处理。**
-
-实现细节： 和上一个差不多，一样是Scrapy对接Selenium，不同的是格外实现了元素加载等待和元素点击事件。
 
 ## 关于远程部署到Linux上的建议
 
---
+++ cnki/test_untitled.py
@@ -0,0 +1,198 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+import requests
+from lxml import etree
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.setWindowModality(QtCore.Qt.NonModal)
+        MainWindow.resize(800, 600)
+        MainWindow.setMinimumSize(QtCore.QSize(800, 600))
+        MainWindow.setMaximumSize(QtCore.QSize(800, 600))
+        font = QtGui.QFont()
+        font.setFamily("微软雅黑")
+        font.setPointSize(9)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setUnderline(False)
+        font.setWeight(50)
+        font.setStrikeOut(False)
+        font.setKerning(True)
+        MainWindow.setFont(font)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(130, 20, 541, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.lineEdit.setFont(font)
+        self.lineEdit.setObjectName("lineEdit")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(20, 10, 111, 51))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(680, 20, 101, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton.clicked.connect(self.on_click)
+        self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser.setGeometry(QtCore.QRect(20, 60, 761, 511))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.textBrowser.setFont(font)
+        self.textBrowser.setObjectName("textBrowser")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 23))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+        self.thread = Worker()
+        self.thread.sinOut.connect(self.slotAdd)
+
+    def on_click(self):
+        self.pushButton.setEnabled(False)
+        self.textBrowser.append('请稍等片刻......')
+        key_word = self.lineEdit.text()
+        self.thread.key_word = key_word
+        self.thread.start()
+
+    def slotAdd(self, file_inf):
+        self.textBrowser.append(file_inf)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "知网爬虫"))
+        self.lineEdit.setText(_translate("MainWindow", "钒钛"))
+        self.label.setText(_translate("MainWindow", "爬取关键字:"))
+        self.pushButton.setText(_translate("MainWindow", "开始爬取"))
+
+
+class Worker(QtCore.QThread):
+    sinOut = QtCore.pyqtSignal(str)
+
+    def __init__(self, parent=None):
+        super(Worker, self).__init__(parent)
+        self.key_word = ''
+        self.working = True
+        self.num = 0
+
+    def __del__(self):
+        self.working = False
+        self.wait()
+
+    def run(self):
+        if self.working == True:
+            self.getstartpaqu(self.key_word)
+
+    def getstartpaqu(self, key_word):
+        self.i=1
+        fireFoxOptions = webdriver.FirefoxOptions()
+        fireFoxOptions.set_headless()
+        self.driver = webdriver.Firefox(executable_path=".\geckodriver.exe", firefox_options=fireFoxOptions)
+        
+        self.driver.get("https://www.cnki.net/")
+        self.driver.find_element(By.ID, "txt_SearchText").click()
+        self.driver.find_element(By.ID, "txt_SearchText").send_keys(key_word)
+        self.driver.find_element(By.ID, "txt_SearchText").send_keys(Keys.ENTER)
+        time.sleep(2.5)
+
+        for a in range(1,200): #232
+            url = "https://kns.cnki.net/kns/brief/brief.aspx?curpage="+str(a)+"&RecordsPerPage=20&QueryID=9&ID=&turnpage=1&tpagemode=L&dbPrefix=SCDB&Fields=&DisplayMode=listmode&PageName=ASP.brief_default_result_aspx&isinEn=1&"
+            # print('【输出】'+url)
+            self.driver.get(url)
+            time.sleep(1)
+            parser = GETHREFHTMLParser()
+            parser.feed(self.driver.page_source)
+            # print('【输出】'+parser.urls)
+            self.sonhtmlgetdata(parser.urls)
+            parser.clearurl
+
+    def sonhtmlgetdata(self, urllist): #用于处理子网页
+        for url in urllist:
+            title = ''
+            abstract = ''
+            keys = []
+            papers = []
+            abstract_data = []
+            keys_data = []
+
+            r = requests.get(url)
+            time.sleep(1)
+            tree=etree.HTML(r.text)
+            title = tree.xpath('//h2[@class="title"]/text()')[0]
+            abstract_data = tree.xpath('//span[@id="ChDivSummary"]/text()')
+            papers = tree.xpath('//div[@class="author"]/span/a/text()')
+            abstract = ''.join(abstract_data).replace('\n', '')
+            if tree.xpath('//div[@class="wxBaseinfo"]/p[2]/label/text()')[0] == '关键词：':
+                keys_data = tree.xpath('//div[@class="wxBaseinfo"]/p[2]/a/text()')
+            if tree.xpath('//div[@class="wxBaseinfo"]/p[2]/label/text()')[0] == '基金：':
+                keys_data = tree.xpath('//div[@class="wxBaseinfo"]/p[3]/a/text()')
+            if papers == []:
+                papers = tree.xpath('//div[@class="author"]/span/text()')
+            keys = []
+            for key in keys_data:
+                keys.append(key.strip().strip(';'))
+            # print(i)
+            # print('【输出】'+title)
+            # print('【输出】'+abstract)
+            # print('【输出】'+str(keys))
+            # print('【输出】'+str(papers))
+            self.sinOut.emit('【序号】'+str(self.i))
+            self.sinOut.emit('【标题】'+title)
+            self.sinOut.emit('【作者】'+str(papers))
+            self.sinOut.emit('【摘要】'+abstract)
+            self.sinOut.emit('【关键字】'+str(keys))
+            self.sinOut.emit('\n')
+            self.i+=1
+
+from html.parser import HTMLParser
+
+class GETHREFHTMLParser(HTMLParser): #处理<a>标签内链接
+    urls = []
+    def handle_starttag(self, tag, attrs):
+        if tag == 'a' and ('class','fz14') in attrs:
+            for attr in attrs:
+                if attr[0] == 'href':
+                    url='https://kns.cnki.net/KCMS' + attr[1][4:]
+                    # print('【输出】'+url)
+                    self.urls.append (url)
+
+    def clearurl(self):
+        self.urls = []
+        self.reset()
+
+import time
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.keys import Keys
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
+
+
+
+
+
+
